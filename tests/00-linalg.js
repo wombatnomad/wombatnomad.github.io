4ae83bbe85c36766baeb72b1f83ac91b26d990bb
Wombat Nomad <98280820+wombatnomad@users.noreply.github.com>
2022-01-30 15:21:25 -0800

    lilb/linalg.js: a few more matrix operations

--- tests/00-linalg.js
+++ tests/00-linalg.js
@@ -29,3 +29,42 @@ assert.eq(
         [1, 4],
         [2, 5],
         [3, 6]));
+
+assert.eq(
+    Matrix.fromRows(
+        [1, 2, 3],
+        [4, 5, 6],
+    ).add(Matrix.fromRows(
+        [5, 6, 7],
+        [8, 9, 10],
+    )),
+    Matrix.fromRows(
+        [6, 8, 10],
+        [12, 14, 16],
+    ),
+);
+
+assert.eq(
+    Matrix.fromRows(
+        [1, 2, 3],
+        [4, 5, 6],
+    ).subtract(Matrix.fromRows(
+        [5, 6, 7],
+        [8, 9, 10],
+    )),
+    Matrix.fromRows(
+        [-4, -4, -4],
+        [-4, -4, -4],
+    ),
+);
+
+assert.eq(
+    Matrix.fromRows(
+        [77, 88, 99],
+        [6, 8, 10],
+    ).scale(1.5),
+    Matrix.fromRows(
+        [115.5, 132, 148.5],
+        [9, 12, 15],
+    )
+);
