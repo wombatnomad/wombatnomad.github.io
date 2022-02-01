10668ff753da5bc3cf85f69702c0eb36e96210ba
Wombat Nomad <98280820+wombatnomad@users.noreply.github.com>
2022-01-31 22:12:53 -0800

    lib/repr.js

---
+++ tests/00-repr.js
@@ -0,0 +1,20 @@
+import assert from "../lib/assert.js";
+import { repr } from "../lib/repr.js";
+
+
+
+assert.eq(repr('hello'), "\"hello\"");
+assert.eq(repr([1, 2, 3]), '[1, 2, 3]');
+assert.eq(repr([1, 2, 3, 'hello']), '[1, 2, 3, \"hello\"]');
+
+
+class CustomToString {
+    toString() {
+        return '<some-custom-toString>';
+    }
+}
+assert.eq(repr(new CustomToString()), '<some-custom-toString>');
+
+
+class NoCustomToString { }
+assert.eq(repr(new NoCustomToString()), '[object Object]');
