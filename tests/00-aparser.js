d09cf0f18b521debfa4c390ad5ae066d7710e242
Wombat Nomad <98280820+wombatnomad@users.noreply.github.com>
2022-02-03 00:05:15 -0800

    aparser: direct left recursive productions

--- tests/00-aparser.js
+++ tests/00-aparser.js
@@ -51,3 +51,33 @@ assert.throws(AssertionError, () => {
         rules: [{ lhs: '#', rhs: ['#x'] }],
     });
 });
+
+{
+    // direct left recursive grammar
+    const parser = new AParser({
+        start: '#',
+        rules: [
+            { lhs: '#', rhs: ['#expr_'] },
+            { lhs: '#expr_', rhs: ['#expr'], throwOnFail: true },
+            { lhs: '#atom_', rhs: ['#atom'], throwOnFail: true },
+            { lhs: '#product_', rhs: ['#product'], throwOnFail: true },
+            { lhs: '#atom', rhs: ['NUMBER'] },
+            { lhs: '#atom', rhs: ['(', '#expr_', ')'], f: (_, x) => x },
+            { lhs: '#atom', rhs: ['-', '#atom_'], f: (_, x) => -x },
+            { lhs: '#atom', rhs: ['+', '#atom_'], f: (_, x) => x },
+            { lhs: '#product', rhs: ['#atom'] },
+            { lhs: '#product', rhs: ['#product', '*', '#atom_'], f: (a, _, b) => a * b },
+            { lhs: '#product', rhs: ['#product', '/', '#atom_'], f: (a, _, b) => a / b },
+            { lhs: '#sum', rhs: ['#product'] },
+            { lhs: '#sum', rhs: ['#sum', '+', '#product_'], f: (a, _, b) => a + b },
+            { lhs: '#sum', rhs: ['#sum', '-', '#product_'], f: (a, _, b) => a - b },
+            { lhs: '#expr', rhs: ['#sum'] },
+        ],
+    });
+    assert.eq(parser.parseValue('2'), 2);
+    assert.eq(parser.parseValue('2 + 7'), 9);
+    assert.eq(parser.parseValue('2 + 7 * 8'), 2 + 7 * 8);
+    assert.eq(parser.parseValue('(2 + 7) * 8'), (2 + 7) * 8);
+    assert.eq(parser.parseValue('(2 + -7) * 8'), (2 + -7) * 8);
+    assert.throws(Error, () => parser.parseValue('(2 + -x) * 8'));
+}
