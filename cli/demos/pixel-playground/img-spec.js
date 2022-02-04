c0b643d7601857077c9aa78cc98b62c390b5fe47
Wombat Nomad <98280820+wombatnomad@users.noreply.github.com>
2022-02-03 20:52:13 -0800

    lib/pixel-playground: equations

--- cli/demos/pixel-playground/img-spec.js
+++ cli/demos/pixel-playground/img-spec.js
@@ -10,3 +10,10 @@ fs.writeFileSync('out.bmp', bmp.toBMPBytes());
 fs.writeFileSync('out2.bmp', ImageSpec.fromSource(`
 bg 255 255 20
 `).toBitmap().toBMPBytes());
+
+fs.writeFileSync('out3.bmp', ImageSpec.fromSource(`
+bg 255 255 20
+fg 20 20 20
+eq y = x * x
+eq y < 0
+`).toBitmap().toBMPBytes());
